a227fcfabca8f965fcd931fd5f79c6c0aa1383fc
eldar.jah <eldar.jah@gmail.com>
2018-07-21 10:39:01 +0200

    Improving comments controller and model. Adding some layout images.

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use App\Article;
+
+class CommentsController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeOne(Article $article)
+    {
+        $this->validate(request(), [
+            'body' => 'required|min:5|max:150'
+        ]);
+
+        Comment::create([
+            'body' => request('body'), 
+            'article_id' => $article->id
+        ]);
+
+        // redirect to index view
+        return back();
+    }
+
+    /**
+     * Stores a comment using eloquent article
+     * @param  Article $article 
+     * @return helper           back to where form was posted from
+     */
+    public function store(Article $article)
+    {
+        $this->validate(request(), [
+            'body' => 'required|min:5|max:150'
+        ]);
+
+        $article->addComment(request('body'));
+
+        return back();
+    }
+}
---
+++ resources/views/comments/create.blade.php
@@ -0,0 +1,12 @@
+<form method="POST" action="/articles/{{ $article->id }}">
+  {{ csrf_field() }}
+  <div class="form-group">
+    <label for="commentInput">Leave a comment</label>
+    <textarea class="form-control" id="commentInput" name="body" aria-describedby="bodyHelp" placeholder="Enter comment text" required></textarea>
+    <small id="bodyHelp" class="form-text text-muted">Comment text</small>
+  </div>
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">Write</button>
+  </div>
+  @include ('layouts.errors')
+</form>
