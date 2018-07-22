4c05eec9027d8ce29c76b2cb071c69cd802008fd
eldar.jah <eldar.jah@gmail.com>
2018-07-22 18:23:58 +0200

    Handling login and register.

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class RegistrationController extends Controller
 {
+	function __construct()
+	{
+		$this->middleware('guest');
+	}
+	
     public function create()
     {
-    	return view('sessions.create');
+    	return view('registration.create');
+    }
+
+    public function store()
+    {
+    	// Validate form
+    	$this->validate(request(), [
+    		'name' => 'required',
+    		'email' => 'required|email',
+    		'password' => 'required|confirmed'
+    	]);
+    	
+    	// Create and save user
+    	$user = User::create(request(['name', 'email', 'password']));
+
+    	// Signin user
+    	auth()->login($user);
+
+    	// Redirect to home page
+    	return redirect()->home();
     }
 }
--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
-    //
+
+	function __construct()
+	{
+		$this->middleware('guest', ['except' => 'destroy']);
+	}
+
+    public function login()
+    {
+    	return view('sessions.create');
+    }
+
+    public function store()
+    {
+    	// Attempt to auth. the user
+    	if(! auth()->attempt(request(['email', 'password'])) ) {
+			return back()->withErrors([
+				'message' => 'Please check your credentials and try again.'
+			]);
+		}
+
+    	// If so, sign in.
+
+    	// If not, redirect to login
+    	return redirect()->home();
+    }
+
+    public function destroy()
+    {
+    	auth()->logout();
+
+    	return redirect()->home();
+    }
 }
