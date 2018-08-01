9dab5c307eafd042074538a35518af75c2725b05
eldar.jah <eldar.jah@gmail.com>
2018-08-02 00:57:38 +0200

    Routes. Mainly routing regarding chatusers, profiles, friends and friend relations.

--- routes/web.php
+++ routes/web.php
@@ -59,10 +59,15 @@ Route::post('/profile/update', 'ChatUserController@update')->name('profile.updat
 
 /*
 |--------------------------------------------------------------------------
-| PROFILE
+| FRIENDS
 |--------------------------------------------------------------------------
 */
 Route::get('/friends', 'FriendsController@index')->name('friends')->middleware('auth.chat');
+
+Route::get('/friends/requests', 'FriendsController@requests')->name('friends.requests')->middleware('auth.chat');
+Route::get('/friends/settings/{chatUser1}/{chatUser2}', 'FriendsController@friendship_settings')->name('friends.friendship_settings')->middleware('auth.chat');
+Route::get('/friends/confirm/{friends}', 'FriendsController@confirm')->name('friends.confirm')->middleware('auth.chat');
+
 Route::get('/friends/{chatUser}', 'FriendsController@add')->name('friends.add')->middleware('auth.chat');
 Route::get('/friends/find', 'FriendsController@find')->name('friends.find')->middleware('auth.chat');
 
